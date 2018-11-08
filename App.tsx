93833aa06988622d314ed08a70f882e957ec81c6
Jon Edwards <jkedwards@me.com>
2018-11-07 17:10:08 -0800

    apply initial TypeScript migration
    add tsconfig.json
    add TypeScript transformer
    add notes to README.md

--- App.tsx
+++ App.tsx
@@ -1,12 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
+import * as React from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
@@ -17,11 +9,11 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+export default class App extends React.Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native !</Text>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # graphRP Clients (Android, iOS, and Web)
+
+## Requirements
+
+* make sure that Chrome is installed (for console debugging of the Android and iOS apps)
+* install Java 8 so you can build the Android app
+* install Android Studio Manager (or the Android CLI SDK tools) – either way, you'll have to accept the licenses
+* install Xcode 10+ so you can build the iOS app
